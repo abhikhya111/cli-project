5240357bd0f6a690c62bd2c3bbee2f0cfcc72ffe
abhikhya111 <abhikhya.ashi@gmail.com>
2022-06-28 16:21:17 +0530

    first commit

---
+++ bin/index.js
@@ -0,0 +1,28 @@
+#!/usr/bin/env node
+
+const chalk = require("chalk");
+const boxen = require("boxen");
+const yargs = require("yargs");
+const axios = require("axios");
+
+const options = yargs
+ .usage("Usage: -n <name>")
+ .option("n", { alias: "name", describe: "Your name", type: "string", demandOption: true })
+ .argv;
+
+const greeting = `Hello, ${options.name}!`;
+
+console.log(greeting);
+
+// const greeting = chalk.white.bold("Hello!");
+
+// const boxenOptions = {
+//  padding: 1,
+//  margin: 1,
+//  borderStyle: "round",
+//  borderColor: "green",
+//  backgroundColor: "#FFC0CB"
+// };
+// const msgBox = boxen( greeting, boxenOptions );
+
+// console.log(msgBox);
